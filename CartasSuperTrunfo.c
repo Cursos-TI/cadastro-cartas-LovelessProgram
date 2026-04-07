13f373a53aaff601750f6573f1fc552937be1269
LovelessProgram <boywan9153@gmail.com>
2026-04-07 14:32:29 +0000

    Adicionado opções de densidade populacional e PIB per capita - Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,11 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   int populacao, pontos;
-  char estado[3], codigo[5], cidade[20];
+  char estado[3], codigo[10], cidade[20];
   float area, pib;
-  // Área para entrada de dados
+  float densidade, percapita;
+
+  // Área para entrada de dados da carta 1:
   printf("Digite o Estado da Carta 1: ");
   scanf("%s", estado);
 
@@ -31,16 +33,25 @@ int main() {
   printf("Digite o Número de Pontos Turísticos da Carta 1: ");
   scanf("%d", &pontos);
 
-      // Área para exibição dos dados da cidade
-  printf("Carta 1:\n" );
+
+      // Área para exibição dos dados da cidade:
+  printf("***Carta 1:***\n" );
   printf("Estado: %s\n", estado);
   printf("Código: %s\n", codigo);
   printf("Nome da Cidade: %s\n", cidade);
   printf("População: %d habitantes\n", populacao);
   printf("Área: %.2f km²\n", area);
-  printf("PIB: %.2f milhões\n", pib);
+  printf("PIB: %.2f bilhões de reais\n", pib);
   printf("Número de Pontos Turisticos: %d\n", pontos);
 
+   // Adição de novas funções - Nível Aventureiro
+  densidade = (float) populacao / area;
+  percapita = (pib * 1000000000) / populacao;
+
+  printf("Densidade populacional: %.2f hab/km²\n", densidade);
+  printf("PIB per capita: %.2f reais\n", percapita);
+  
+  // Área para adicionar os dados da carta 2:
   printf("Digite o Estado da Carta 2: ");
   scanf("%s", estado);
 
@@ -64,14 +75,21 @@ int main() {
 
   // Área para exibição dos dados da cidade
 
-  printf("Carta 2:\n" );
+  printf("***Carta 2:***\n" );
   printf("Estado: %s\n", estado);
   printf("Código: %s\n", codigo);
   printf("Nome da Cidade: %s\n", cidade);
   printf("População: %d habitantes\n", populacao);
   printf("Área: %.2f km²\n", area);
-  printf("PIB: %.2f milhões\n", pib);
+  printf("PIB: %.2f bilhões de reais\n", pib);
   printf("Número de Pontos Turisticos: %d\n", pontos);
+
+  // Adição de novas funções - Nível Aventureiro
+  densidade = (float) populacao / area;
+  percapita = (pib * 1000000000) / populacao;
+
+  printf("Densidade populacional: %.2f hab/km²\n", densidade);
+  printf("PIB per capita: %.2f reais\n", percapita);
 
 return 0;
 } 
